c2c93b64573154de8cdf4c4d669c4e6189aae79b
Rand3rino <nguyeran@mail.gvsu.edu>
2024-01-17 20:19:22 -0500

    Load N1 Count by Site

--- WBE-Analysis/app.R
+++ WBE-Analysis/app.R
@@ -7,27 +7,34 @@
 #    https://shiny.posit.co/
 #
 
+
+## https://debruine.github.io/shinyintro/data.html
 library(shiny)
+library(tidyverse)
+library(plotly)
+library(googlesheets4)
+gs4_deauth()
+sheet_id <- "https://docs.google.com/spreadsheets/d/1Y8HZf93GiC_8XjK7nxqZONcTmi4Ck7EH96hR4q6Kbio"
+N1counts <- read_sheet(sheet_id)
+N1counts$Date <- as.Date(as.character(N1counts$Date), "%y%m%d")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("WBE-Analysis"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          selectInput("SiteSelect",
+                        "Select Site to view",
+                        choices = c("CS", "GG", "GO", "GR", "WB", "WK", "WY"))
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           plotOutput("N1Plot")
         )
     )
 )
@@ -35,15 +42,9 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    output$N1Plot <- renderPlot({
+        N1Site <- N1counts %>% filter(Site == input$SiteSelect)
+        ggplot(N1Site, aes(x=Date, y=`N1 GC/100mL`)) + geom_line() + geom_point() + theme_bw()
     })
 }
 
