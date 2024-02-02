440d4283ac1fcfa64a1c9d956938e4f170773cc0
Rand3rino <nguyeran@mail.gvsu.edu>
2024-02-01 23:39:56 -0500

    Add variant proportion plot

--- WBE-Analysis/app.R
+++ WBE-Analysis/app.R
@@ -1,7 +1,4 @@
-# Forecasting Sandbox ----
-# This is an example for a Shinylive R app
-# The app provides a forecasting sandbox for the AirPassengers dataset
-# It supports 3 stats forecasting models - Linear Regression, ARIMA, and Holt-Winters
+
 
 library(shiny)
 library(tidyverse)
@@ -29,6 +26,12 @@ library(tidyverse)
 # https://rpubs.com/kylewbrown/github-csv-r
 # N1counts <- read.csv(github_url)
 N1counts <- read.csv("https://docs.google.com/spreadsheets/d/1Y8HZf93GiC_8XjK7nxqZONcTmi4Ck7EH96hR4q6Kbio/export?format=csv")
+N1counts$Date <- as.Date(as.character(N1counts$Date), format = "%y%m%d")
+
+VariantProportions <- read.csv("https://docs.google.com/spreadsheets/d/19sCocxxppFfBrM0AKEojOzWoN6C6myoZjIA492deetc/export?format=csv")
+VariantProportions$Date <- as.Date(VariantProportions$Date, format ="%m/%d/%Y")
+VariantProportions <- VariantProportions %>% 
+  pivot_longer(!Date, names_to = "Variant", values_to = "Proportion", values_drop_na = TRUE)
 
 # # Define UI for application that draws a line chart
 ui <- fluidPage(
@@ -37,18 +40,19 @@ ui <- fluidPage(
   titlePanel("WBE-Analysis"),
   
   # Sidebar with a dropdown for number site
-  sidebarLayout(
-    sidebarPanel(
+  # sidebarLayout(
+    # sidebarPanel(
       # selectInput("SiteSelect",
       #             "Select Site to view",
       #             choices = c("CS", "GG", "GO", "GR", "WB", "WK", "WY"))
-    ),
+    # ),
     
     # Show a plot of the n1 counts
     mainPanel(
-      plotOutput("N1Plot")
+      plotOutput("N1Plot"),
+      plotOutput("VariantPlot")
     )
-  )
+  # )
 )
 # 
 # # Define server logic required to draw a line chart
@@ -56,7 +60,12 @@ server <- function(input, output) {
   
   output$N1Plot <- renderPlot({
     N1Site <- N1counts #%>% filter(Site == input$SiteSelect)
-    ggplot(N1Site, aes(x=Date, y=`N1.GC.100mL`)) + geom_line() + geom_point() + theme_bw()
+    ggplot(N1Site, aes(x=Date, y=log(`N1.GC.100mL`))) + geom_point() + geom_smooth() + theme_bw() #+ geom_line() 
+  })
+  
+  output$VariantPlot <- renderPlot({
+    Variants <- VariantProportions #%>% filter(Site == input$SiteSelect)
+    ggplot(Variants, aes(x=Date, y=Proportion)) + geom_col(aes(fill=Variant)) + theme_bw() + theme(legend.position = "bottom")
   })
 }
 
