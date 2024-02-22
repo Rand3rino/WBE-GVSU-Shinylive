868792f208557a6d9ac4c858d4e8ff945b219d52
Rand3rino <nguyeran@mail.gvsu.edu>
2024-02-21 22:28:44 -0500

    Proportion chart, order by variant appearance

--- WBE-Analysis/app.R
+++ WBE-Analysis/app.R
@@ -2,15 +2,20 @@
 
 library(shiny)
 library(tidyverse)
-# library(plotly)
+library(plotly)
 
 N1counts <- read.csv("https://docs.google.com/spreadsheets/d/1Y8HZf93GiC_8XjK7nxqZONcTmi4Ck7EH96hR4q6Kbio/export?format=csv")
 N1counts$Date <- as.Date(as.character(N1counts$Date), format = "%y%m%d")
 
 VariantProportions <- read.csv("https://docs.google.com/spreadsheets/d/19sCocxxppFfBrM0AKEojOzWoN6C6myoZjIA492deetc/export?format=csv")
 VariantProportions$Date.by.Week <- as.Date(VariantProportions$Date.by.Week, format ="%m/%d/%Y")
+colnames(VariantProportions) <- paste(colnames(VariantProportions), match(colnames(VariantProportions), colnames(VariantProportions)), sep="_")
 VariantProportions <- VariantProportions %>%
-  pivot_longer(!Date.by.Week, names_to = "Variant", values_to = "Proportion", values_drop_na = TRUE)
+  pivot_longer(!Date.by.Week_1, names_to = "Variant", values_to = "Proportion", values_drop_na = TRUE)
+VariantProportions <- separate_wider_delim(VariantProportions, Variant, delim = "_", names = c("Variant", "Appearance"))
+VariantProportions$Appearance <- as.integer(VariantProportions$Appearance)
+VariantProportions <- VariantProportions[order(VariantProportions$Appearance),]
+VariantProportions$Variant <- factor(VariantProportions$Variant, levels=unique(VariantProportions$Variant))
 
 # # Define UI for application that draws a line chart
 ui <- fluidPage(
@@ -28,10 +33,10 @@ ui <- fluidPage(
     
     # Show a plot of the n1 counts
     mainPanel(
-      # plotlyOutput("N1Plot"),
-      # plotlyOutput("VariantPlot")
-      plotOutput("N1Plot"),
-      plotOutput("VariantPlot")
+      plotlyOutput("N1Plot"),
+      plotlyOutput("VariantPlot")
+      # plotOutput("N1Plot"),
+      # plotOutput("VariantPlot")
     )
   # )
 )
@@ -39,21 +44,26 @@ ui <- fluidPage(
 # # Define server logic required to draw a line chart
 server <- function(input, output) {
   
-  # output$N1Plot <- renderPlotly({
-  #   ggplotly(ggplot(N1counts, aes(x=Date, y=log(N1.GC.100mL))) + geom_point())
+  output$N1Plot <- renderPlotly({
+    ggplotly(ggplot(N1counts, aes(x=Date, y=log(N1.GC.100mL))) + geom_point() + theme_bw())
+  })
+
+  output$VariantPlot <- renderPlotly({
+    ggplotly(ggplot(VariantProportions, aes(x=Date.by.Week_1, y=Proportion)) 
+      + geom_col(aes(fill=Variant, color=Variant))
+      + theme_bw()
+      + scale_y_continuous(labels = scales::percent) # Y-Axis as percents
+      + xlab("Date")
+    )
+  })
+  
+  # output$N1Plot <- renderPlot({
+  #   ggplot(N1counts, aes(x=Date, y=log(N1.GC.100mL))) + geom_point() + geom_smooth() + theme_bw()
   # })
   # 
-  # output$VariantPlot <- renderPlotly({
-  #   ggplotly(ggplot(VariantProportions, aes(x=Date.by.Week, y=Proportion)) + geom_col(aes(fill=Variant)) + theme_bw())
+  # output$VariantPlot <- renderPlot({
+  #   ggplot(VariantProportions, aes(x=Date.by.Week, y=Proportion)) + geom_col(aes(fill=Variant)) + theme_bw() + theme(legend.position = "bottom")
   # })
-  
-  output$N1Plot <- renderPlot({
-    ggplot(N1counts, aes(x=Date, y=log(N1.GC.100mL))) + geom_point() + geom_smooth() + theme_bw()
-  })
-  
-  output$VariantPlot <- renderPlot({
-    ggplot(VariantProportions, aes(x=Date.by.Week, y=Proportion)) + geom_col(aes(fill=Variant)) + theme_bw() + theme(legend.position = "bottom")
-  })
 }
 
 # Run the application 
